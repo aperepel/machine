c59f330c1d36f8b8adbe56e8864294abc46153fe
aperepel <andrew@hortonworks.com>
2015-06-08 19:46:44 -0400

    Fixes #1243 Custom GCE images support
    Signed-off-by: Andrew Grande <aprepel@gmail.com>

--- drivers/google/compute_util.go
+++ drivers/google/compute_util.go
@@ -29,8 +29,9 @@ type ComputeUtil struct {
 }
 
 const (
-	apiURL             = "https://www.googleapis.com/compute/v1/projects/"
-	imageName          = "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20150316"
+	apiURL           = "https://www.googleapis.com/compute/v1/projects/"
+	defaultImageName = "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20150316"
+	//	imageName          = "https://www.googleapis.com/compute/v1/projects/centos-cloud/global/images/centos-7-v20150603"
 	firewallRule       = "docker-machines"
 	port               = "2376"
 	firewallTargetTag  = "docker-machine"
@@ -181,6 +182,10 @@ func (c *ComputeUtil) createInstance(d *Driver) error {
 	}
 	disk, err := c.disk()
 	if disk == nil || err != nil {
+		imageName := defaultImageName
+		if d.MachineImage != "" {
+			imageName = d.MachineImage
+		}
 		instance.Disks[0].InitializeParams = &raw.AttachedDiskInitializeParams{
 			DiskName:    c.diskName(),
 			SourceImage: imageName,
